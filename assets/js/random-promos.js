83f5befc11219f6c00c37784d62aad353871eb55
johnhopes-collab <mail.hopes.john@gmail.com>
2026-05-30 14:08:25 -0500

    Update random-promos.js

--- assets/js/random-promos.js
+++ assets/js/random-promos.js
@@ -40,6 +40,18 @@ document.addEventListener('DOMContentLoaded', function() {
             link: 'https://grimmsdetectiveagency.com/archives/cars.html',
             alt: 'Speed Promo'
         },
+        {
+            id: 'newspaper1',
+            path: '/assets/images/sq/sq_h_newspaper1.png',
+            link: 'https://grimmsdetectiveagency.com/archives/newspaper.html',
+            alt: 'Newspaper Promo 1'
+        },
+        {
+            id: 'newspaper2',
+            path: '/assets/images/sq/sq_h_newspaper2.png',
+            link: 'https://grimmsdetectiveagency.com/archives/newspaper.html',
+            alt: 'Newspaper Promo 2'
+        },
     ];
     
     // Get previously displayed promos from localStorage
